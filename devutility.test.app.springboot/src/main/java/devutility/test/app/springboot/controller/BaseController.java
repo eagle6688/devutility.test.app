2101fe7101b7a611a915e2a85a83012883e3c8f6
Aldwin Su <fangyansu@163.com>
2018-09-05 18:30:18 +0800

    Add Log4j2 in springboot

---
+++ devutility.test.app.springboot/src/main/java/devutility/test/app/springboot/controller/BaseController.java
@@ -0,0 +1,8 @@
+package devutility.test.app.springboot.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class BaseController {
+	protected static Logger logger = LoggerFactory.getLogger(BaseController.class);
+}
