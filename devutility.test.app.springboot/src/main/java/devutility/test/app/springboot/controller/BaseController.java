2cf4b48e9120bf98f36d301a0234b11ec695acf5
Aldwin Su <fangyansu@163.com>
2018-09-06 14:19:58 +0800

    Move all about log4j2 to devutility.test.log

--- devutility.test.app.springboot/src/main/java/devutility/test/app/springboot/controller/BaseController.java
+++
@@ -1,8 +0,0 @@
-package devutility.test.app.springboot.controller;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public abstract class BaseController {
-	protected static Logger logger = LoggerFactory.getLogger(BaseController.class);
-}
