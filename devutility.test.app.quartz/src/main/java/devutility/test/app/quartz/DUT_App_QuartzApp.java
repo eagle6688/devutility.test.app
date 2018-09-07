77850b889ce741e5a2c62d1068988ec1539c68fc
Aldwin Su <fangyansu@163.com>
2018-09-07 16:15:56 +0800

    Add sub-project devutility.test.app.quartz

---
+++ devutility.test.app.quartz/src/main/java/devutility/test/app/quartz/DUT_App_QuartzApp.java
@@ -0,0 +1,11 @@
+package devutility.test.app.quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DUT_App_QuartzApp {
+	public static void main(String[] args) {
+		SpringApplication.run(DUT_App_QuartzApp.class, args);
+	}
+}
