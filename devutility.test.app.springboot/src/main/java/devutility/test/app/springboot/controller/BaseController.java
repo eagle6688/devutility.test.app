fa555b4665b0d22b9769b14468a903580bd45d0f
Aldwin Su <fangyansu@163.com>
2018-11-09 17:25:57 +0800

    Solve the pre submit.

--- devutility.test.app.springboot/src/main/java/devutility/test/app/springboot/controller/BaseController.java
+++ devutility.test.app.springboot/src/main/java/devutility/test/app/springboot/controller/BaseController.java
@@ -3,27 +3,12 @@ package devutility.test.app.springboot.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public abstract class BaseController {
-	/**
-	 * Request
-	 */
+	@Autowired
 	protected HttpServletRequest request;
 
-	/**
-	 * Response
-	 */
+	@Autowired
 	protected HttpServletResponse response;
-
-	/**
-	 * Set context.
-	 * @param request
-	 * @param response
-	 */
-	@ModelAttribute
-	public void setContext(HttpServletRequest request, HttpServletResponse response) {
-		this.request = request;
-		this.response = response;
-	}
 }
